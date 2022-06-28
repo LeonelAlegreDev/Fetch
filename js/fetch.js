c21ae286f04d25f9ff0f22aca439e56c3ab82b04
Leonel Alegre <leonelalegre.dev@gmail.com>
2022-06-28 14:33:05 -0300

    Fetch practice

---
+++ js/fetch.js
@@ -0,0 +1,28 @@
+const ul = document.getElementById('list');
+const url = 'https://randomuser.me/api/?results=10';
+
+function createNode(element){
+    return document.createElement(element);
+}
+function append(parent, element){
+    return parent.appendChild(element);
+}
+
+fetch(url)
+.then((resp) => resp.json())
+    .then(function(data){
+        let authors = data.results;
+        return authors.map(function(author){
+            let li = createNode('li');
+            let img = createNode('img');
+            let span = createNode('span');
+            img.src = author.picture.medium;
+            span.innerHTML = `${author.name.first} ${author.name.last}`;
+            append(li, img);
+            append(li, span);
+            append(ul, li); 
+        })
+    })
+.catch( (error) => {
+    console.log(error);
+});
